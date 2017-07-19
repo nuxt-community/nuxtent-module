ea273d88bbbc535fcf16be98f61a647252f4a8eb
Alid Castano <alidcastano@gmail.com>
2017-07-19 13:37:33 -0400

    fixed link

--- docs/pages/index/index.vue
+++ docs/pages/index/index.vue
@@ -8,7 +8,7 @@ div.lead-container
     nuxt-link.lead-link(to="/introduction") Get Started
   div.featured-lesson
     h3 Featured Lesson:
-    a(href="/writing-content#async-components")
+    a(href="/writing#async-components")
       h1.lesson-title Use Vue Components in Your Markdown Files!
 </template>
 
