1deaf6d3a9ae2b6da562d3381a331275540e0e05
Alid Castano <alidcastano@gmail.com>
2017-08-04 14:55:08 -0400

    setup tests #37

---
+++ test/unit/karma.conf.js
@@ -0,0 +1,26 @@
+// karma.conf.js
+module.exports = function (config) {
+	config.set({
+		frameworks: ['mocha', 'sinon-chai'],
+
+		files: [
+			'./specs/**/*.spec.js',
+		],
+
+		preprocessors: {
+			'../../lib/module.js': ['rollup'],
+			'../../lib/content/**/*.js': ['rollup'],
+			'./specs/**/*.spec.js': ['rollup'],
+		},
+
+		rollupPreprocessor: {
+			plugins: [
+				require('rollup-plugin-babel')(),
+				require('rollup-plugin-commonjs')()
+			],
+      format: 'umd',
+      moduleName: `nuxtContent`,
+			sourceMap: 'inline'
+		}
+	})
+}
---
+++ test/unit/specs/database.spec.js
@@ -0,0 +1,12 @@
+/* global describe, it, expect, beforeEach, sinon */
+
+import db from 'lib/content/database'
+
+describe('database API', function () {
+
+  it('works', () => {
+    // expect(tasksObj).to.not.throw(Error)
+    // expect(tasksFn).to.not.throw(Error)
+    // expect(tasksList).to.throw(Error)
+  })
+})
