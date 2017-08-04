3b132407b37e9e700b4f0b604c67a408a015d374
Alid Castano <alidcastano@gmail.com>
2017-08-04 18:33:03 -0400

    configurd 2e2 tests

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -1,20 +1,21 @@
-// karma.conf.js
 module.exports = function (config) {
 	config.set({
 		frameworks: ['mocha', 'sinon-chai'],
 
 		files: [
-			'./specs/**/*.spec.js',
+			'./specs/**.spec.js',
 		],
 
 		preprocessors: {
 			'../../lib/module.js': ['rollup'],
-			'../../lib/content/**/*.js': ['rollup'],
-			'./specs/**/*.spec.js': ['rollup'],
+			'../../lib/content/**.js': ['rollup'],
+			'./specs/**.spec.js': ['rollup'],
 		},
 
 		rollupPreprocessor: {
 			plugins: [
+				require('rollup-plugin-node-resolve')(),
+				require('rollup-plugin-node-builtins')(),
 				require('rollup-plugin-babel')(),
 				require('rollup-plugin-commonjs')()
 			],
--- test/unit/specs/database.spec.js
+++ test/unit/specs/database.spec.js
@@ -1,12 +1,19 @@
 /* global describe, it, expect, beforeEach, sinon */
 
-import db from 'lib/content/database'
+import createDatabase from '../../../lib/content/database'
+
+import { resolve } from 'path'
 
 describe('database API', function () {
 
-  it('works', () => {
-    // expect(tasksObj).to.not.throw(Error)
-    // expect(tasksFn).to.not.throw(Error)
-    // expect(tasksList).to.throw(Error)
+  it('loads single content type', () => {
+    const contentPath = '../../fixtures/single-content-type/content'
+    const dirName = '/'
+    const options = {
+      permalink: '/:slug'
+    }
+    const db = createDatabase(contentPath, dirName, options)
+    // TODO
+    // expect().to.not.throw(Error)
   })
 })
