2362b814068f67e40f97fa6b330b4a024970c260
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-09-21 18:40:00 +0200

    chore(build): fix rollup build
    Fix plugins order in `rollup` config
    Add inclusion of `node_modules` to commonjs plugin
    Add commonjs default export wrapper
    Remove fs.existsSync call
    Add `json` plugin in `rollup` config
    Add `babel-plugin-external-helpers` devDependency, add plugin to babel rollup config
    Update `rollup` and `rollup-plugin-babel` devDependencies
    Update config according to new `rollup` version
    Add `express` to globals in rollup config
    Add named `exports` to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve'
+import json from 'rollup-plugin-json'
 import builtins from 'rollup-plugin-node-builtins'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
@@ -9,21 +10,29 @@ import copy from 'rollup-plugin-copy'
 const version = process.env.VERSION || require('./package.json').version
 
 const corePlugins = () => [
-  babel(),
   resolve(),
-  commonjs(),
+  commonjs({
+    include: 'node_modules/**'
+  }),
+  babel({
+    plugins: ['external-helpers']
+  }),
   builtins(),
   uglify(),
   filesize()
 ]
 
 const bundle = (name, options) => ({
-  entry: `lib/${name}.js`,
-  dest: `dist/${name}.js`,
-  format: 'umd',
-  moduleName: `nuxtContent`,
+  input: `lib/${name}.js`,
+  output: {
+    file: `dist/${name}.js`,
+    format: 'cjs',
+    exports: 'named'
+  },
+  name: `nuxtContent`,
   plugins: options.plugins || [],
   external: options.external || [],
+  globals: options.globals || {},
   banner: `
     /**
     * Nuxt Content v${version}
@@ -36,6 +45,7 @@ const bundle = (name, options) => ({
 export default [
   bundle('module', {
     plugins: [
+      json(),
       copy({
         'lib/plugin.js': 'dist/plugin.js',
         'lib/loader.js': 'dist/loader.js'
@@ -45,6 +55,9 @@ export default [
     external: [
       'express',
       'axios'
-    ]
+    ],
+    globals: {
+      express: 'express'
+    }
   })
 ]
