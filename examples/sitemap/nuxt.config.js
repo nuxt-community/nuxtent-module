05c5bc9695577fe9e6768d672d53aa0b93d3192c
Alid Castano <alidcastano@gmail.com>
2017-12-24 18:21:15 -0600

    fixed OS issues

--- examples/sitemap/nuxt.config.js
+++ examples/sitemap/nuxt.config.js
@@ -16,17 +16,23 @@ module.exports = {
   sitemap: {
     generate: true,
     routes: () => {
-      return axios.get('http://localhost:3000/content-api').then(res => {
-        return res.data['content-endpoints']
-      }).then(endpoints => {
-        return Promise.all(endpoints.map(endpoint => {
-          return axios.get(`http://localhost:3000/content-api${endpoint}`)
-        }))
-      }).then(endpoints => {
-        return endpoints.reduce((routes, endpoint) => {
-          return routes.concat(endpoint.data.map(page => page.permalink))
-        }, [])
-      })
+      return axios
+        .get('http://localhost:3000/content-api')
+        .then(res => {
+          return res.data['content-endpoints']
+        })
+        .then(endpoints => {
+          return Promise.all(
+            endpoints.map(endpoint => {
+              return axios.get(`http://localhost:3000/content-api${endpoint}`)
+            })
+          )
+        })
+        .then(endpoints => {
+          return endpoints.reduce((routes, endpoint) => {
+            return routes.concat(endpoint.data.map(page => page.permalink))
+          }, [])
+        })
     }
   }
 }
